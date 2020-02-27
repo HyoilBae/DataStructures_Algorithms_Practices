a1dc2757ed45399916b2c0050ac226ae91f5c252
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-02-27 11:59:56 -0800

    Create HackerRank_ArrayOfSum.cs

---
+++ HackerRank_ArrayOfSum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_ArrayOfSum
+{
+    class Program
+    {
+        static int simpleArraySum(int[] ar)
+        {
+            /*
+             * Write your code here.
+             */
+            int sum = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                sum = sum + ar[i];
+            }
+
+            return sum;
+
+
+        }
+        static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            //int arCount = Convert.ToInt32(Console.ReadLine());
+            int arCount = Convert.ToInt32(Console.ReadLine());
+            //int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+            //int result = simpleArraySum(ar);
+            int result = simpleArraySum(ar);
+            //textWriter.WriteLine(result);
+            Console.WriteLine(result);
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
