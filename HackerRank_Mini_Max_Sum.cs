4deb561c1ce0a07a29e4ea01a27e3ab1d83c06bb
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-08 13:20:42 -0700

    Update HackerRank_Mini_Max_Sum.cs

--- HackerRank_Mini_Max_Sum.cs
+++ HackerRank_Mini_Max_Sum.cs
@@ -11,10 +11,37 @@ namespace HackerRank_Mini_Max_Sum
         static void Main(string[] args)
         {
             int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
-            miniMaxSum(arr);
+            //miniMaxSum(arr);
             //Ex {1, 2, 3, 4, 5}
+            miniMaxSum1(arr);
+            
+            
+        }
+        static void miniMaxSum1(int[] arr)
+        {
+            List<double> intList = new List<double>();
+
+            double sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    sum = sum + arr[j];
+                }
+                sum = sum - arr[i];
+                intList.Add(sum);
+                sum = 0;
+            }
+
+            double max = Math.Round(intList.Max(), 10);
+            double min = Math.Round(intList.Min(), 10);
+
+            Console.WriteLine("{0} {1}", min, max);
+
+
         }
 
+            
         static void miniMaxSum(int[] arr)
         {
             int sum = 0;
@@ -31,13 +58,18 @@ namespace HackerRank_Mini_Max_Sum
                 {
                     max = sum;
                 }
-                
-                
+
+                if (min == 0)
+                {
+                    min = sum;
+                }
+                else if(sum < min)
+                {
+                    min = sum;
+                }
+                sum = 0;
             }
-
             Console.WriteLine("{0} {1}", min, max);
-
-
         }
     }
 }
