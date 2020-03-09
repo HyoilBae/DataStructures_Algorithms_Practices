365f419c8a48fb4bc8615632915e15f5fc98565c
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-09 12:39:47 -0700

    2020_03_09

---
+++ HackerRank_StairCase/HackerRank_StairCase/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_StairCase
+{
+    class Program
+    {
+        static void staircase(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+
+                string space = new string(' ', n - i);
+                Console.Write(space);
+
+                string sharp = new string('#', i);
+                Console.Write(sharp);
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            staircase(n);
+        }
+    }
+}
