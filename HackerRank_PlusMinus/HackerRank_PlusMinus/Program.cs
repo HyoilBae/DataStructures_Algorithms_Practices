202bba94182222277626e4ea2b647d974a8494c7
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-09 11:44:36 -0700

    2020_03_09

---
+++ HackerRank_PlusMinus/HackerRank_PlusMinus/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_PlusMinus
+{
+    class Program
+    {
+        static void plusMinus(int[] arr)
+        {
+            double positive = 0;
+            double negative = 0;
+            double zero = 0;
+            foreach (double i in arr)
+            {
+                if (i > 0)
+                {
+                    positive++;
+                }
+                else if(i == 0)
+                {
+
+                    zero++;
+                }
+                else if(i < 0)
+                {
+                    negative++;
+                }
+
+
+            }
+            double pos = ((double)positive / (double)arr.Length);
+            Console.WriteLine(pos);
+            double neg = Math.Round(((double)negative / (double)arr.Length), 6);
+            Console.WriteLine(neg);
+            double zer = Math.Round(((double)zero / (double)arr.Length), 6);
+            Console.WriteLine(zer);
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            
+            plusMinus(arr);
+        }
+    }
+}
