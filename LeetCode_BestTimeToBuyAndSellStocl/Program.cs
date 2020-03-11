a0d243e5d82e7d7553129e558470e2f89eea2b38
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-11 16:36:00 -0700

    2020_03_11

---
+++ LeetCode_BestTimeToBuyAndSellStocl/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_BestTimeToBuyAndSellStocl
+{
+    class Program
+    {
+        static int maxProfit(int[] prices)
+        {
+            int maxprofit = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > prices[i-1])
+                {
+                    maxprofit += prices[i] - prices[i - 1];
+                }
+            }
+            return maxprofit;
+        }
+        static void Main(string[] args)
+        {
+            int[] userInput = new int[] { 7, 1, 5, 3, 6, 4 };
+
+            Console.WriteLine(maxProfit(new int[] { 7, 1, 5, 3, 6, 4 }));
+        }
+    }
+}
