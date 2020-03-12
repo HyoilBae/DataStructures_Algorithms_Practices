80a62f1355c84685c9a0db2e64ce97aa8217b0ef
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-11 17:06:17 -0700

    2020_03_11

---
+++ LeetCode_BestTimeToBuySellStock/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_BestTimeToBuySellStock
+{
+    class Program
+    {
+        static int maxProfit(int[] prices)
+        {
+            int minPrice = int.MaxValue;
+            int maxProfit = 0;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                //looking for minimun price (valley)
+                if (prices[i] < minPrice)
+                {
+                    minPrice = prices[i];
+                }
+
+                //looking for maximun profit (Peak - Valley)
+                else if (prices[i] - minPrice > maxProfit)
+                {
+                    maxProfit = prices[i] - minPrice;
+                }
+            }
+            return maxProfit;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(maxProfit(new int[] { 7, 1, 5, 3, 6, 4 }));
+        }
+    }
+}
