1f7c62351e7399a23d49aefee3139cb42c49ae52
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-16 06:31:40 -0700

    recursion_factorial

---
+++ _3_3_recursion_factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_3_recursion_factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fact(5));
+        }
+
+        private static int Fact(int x)
+        {
+            if (x <= 1)
+            {
+                return 1;
+            }
+            return x * Fact(x - 1);
+        }
+    }
+}
