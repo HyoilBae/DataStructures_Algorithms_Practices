8fff5c97e119efda92183f13c8e46abcdc90fc7d
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-16 06:32:59 -0700

    quick_sort

---
+++ _4_4_quicksort_recursive_max/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_4_quicksort_recursive_max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Max(new[] { 1, 3, 2, 5, 9, 8 }));
+        }
+
+        private static int Max(IEnumerable<int> list)
+        {
+            if (!list.Any()) throw new ArgumentException(nameof(list));
+            if (list.Count() == 1) return list.First();
+            if (list.Count() == 2) return list.First() > list.Skip(1).Take(1).First() ? list.First() : list.Skip(1).Take(1).First();
+            var sub_max = Max(list.Skip(1));
+            return list.First() > sub_max ? list.First() : sub_max;
+        }
+    }
+}
