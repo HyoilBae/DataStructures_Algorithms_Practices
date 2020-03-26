1ecac936d2331753c799b35d1744bdfa2948de8d
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-26 08:39:22 -0700

    CountTheNumberOfDuplicates

---
+++ CountTheNumberOfDuplicates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountTheNumberOfDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static int DuplicateCount(string str)
+        {
+            return -1;
+        }
+    }
+}
