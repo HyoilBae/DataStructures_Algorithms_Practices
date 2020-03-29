d832975ade38f083dd56a5ff4e017b4381d0f0fc
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-29 08:58:29 -0700

    Var_Example

---
+++ Var_Example/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doubleNumber= 123.3;
+            var intNumber = 123;
+
+            intNumber = Convert.ToInt32(doubleNumber);
+        }
+    }
+}
