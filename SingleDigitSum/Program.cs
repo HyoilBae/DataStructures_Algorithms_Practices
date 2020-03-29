b8c06b806b1f7e03f183defbb351e582a5653a15
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-29 08:57:47 -0700

    SingleDigitSum

---
+++ SingleDigitSum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleDigitSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(" ", singleDigitSum(41)));
+        }
+
+        public static int singleDigitSum(int x)
+        {
+            int sum = 0;
+            int check = 10;
+
+            while (check > 9)
+            {
+                sum = 0;
+                while (x > 0 )
+                {
+                    sum = x % 10 + sum;
+                    x = x / 10;
+                }
+                x = sum;
+                check = sum;
+            }
+            return sum;
+        }
+    }
+}
