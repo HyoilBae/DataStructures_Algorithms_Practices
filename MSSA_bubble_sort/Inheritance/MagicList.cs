2544d3f923973ec4f7f74555598ad99684b747d1
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-29 08:56:55 -0700

    bubble_sort

---
+++ MSSA_bubble_sort/Inheritance/MagicList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class MagicList<T> : List<T>
+    {
+        public T ChooseRandom()
+        {
+            Random randomGenerator = new Random();
+            return this[randomGenerator.Next(0, Count)];
+        }
+    }
+}
