b90a973dd32c0ec41b15ba4c00cabe3985b412fb
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-04-04 12:04:02 -0700

    Enum_Struct

---
+++ Enum_Struct/Enum_Struct/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Struct
+{
+    class Program
+    {
+
+        public struct Student
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public double GPA { get; set; }
+
+            private string _major;
+            public string Major
+            {
+                get { return _major; }
+                set
+                {
+                    if (value == "MSSA")
+                    {
+                        _major = value;
+                    }
+                    else
+                    {
+                        _major = "other";
+                    }
+
+                }
+
+            }
+
+            //public Student(string newFirstName, string newLastName, double newGPA, string newMajor)
+            //{
+            //    this.FirstName = newFirstName;
+            //    this.LastName = newLastName;
+            //    this.GPA = newGPA;
+            //    this.Major = newMajor;
+            //}
+        }
+
+        static void Main(string[] args)
+        {
+            Student alice = new Student();
+            alice.FirstName = "Alice";
+            alice.LastName = "Brown";
+            alice.GPA = 3.56;
+            alice.Major = "MSSA";
+        }
+    }
+}
