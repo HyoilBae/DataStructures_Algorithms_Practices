9c6fa51b3578bf6b5490903e4def6eccf48eff38
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-04-06 18:07:41 -0700

    MSFT_344. Reverse String

---
+++ MSFT_ReverseString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSFT_ReverseString
+{
+    public class Solution
+    {
+        public void ReverseString(string str)
+        {
+            char[] s = str.ToCharArray();
+
+            int left = 0, right = s.Length - 1;
+            while (left < right)
+            {
+                char tmp = s[left];
+                s[left++] = s[right];
+                s[right--] = tmp;
+            }
+            Console.WriteLine(s);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+
+            s.ReverseString("Hello");
+        }
+    }
+}
