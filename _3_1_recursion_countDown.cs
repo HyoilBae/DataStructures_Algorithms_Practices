5c4b34f23759b7f1ab3ae1aca9b85d02e697bf91
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-04-08 08:35:13 -0700

    Recursion

---
+++ _3_1_recursion_countDown.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_1_recursion_countDown
+{
+    class _3_1_recursion_countDown
+    {
+        static void Main(string[] args)
+        {
+            CountDown(5);
+        }
+
+        private static void CountDown(int i)
+        {
+            Console.WriteLine(i);
+
+            //base case
+            if (i <= 0)
+            {
+                return;
+            }
+
+            //recursive case
+            CountDown(i - 1);
+        }
+    }
+}
---
+++ _3_2_recursion_greet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_2_recursion_greet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greet("adit");
+        }
+
+        private static void Greet(string name)
+        {
+            Console.WriteLine($"hello, {name}!");
+            Greet2(name);
+            Console.WriteLine("getting ready to say bye...");
+            Bye();
+        }
+
+        private static void Greet2(string name)
+        {
+            Console.WriteLine($"how are you, {name}?");
+        }
+
+        private static void Bye()
+        {
+            Console.WriteLine("ok bye!");
+        }
+    }
+}
