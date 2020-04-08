5f6ee439ce9d2be70140693a2d0f5a8c9a8f0add
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-04-08 08:35:23 -0700

    Quick_Sort

---
+++ _4_1_quicksort_loop_sum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_1_quicksort_loop_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(new[] { 1, 2, 3, 4 }));
+        }
+
+        private static int Sum(IEnumerable<int> arr)
+        {
+            var total = 0;
+            foreach (var x in arr)
+            {
+                total += x;
+            }
+            return total;
+        }
+    }
+}
---
+++ _4_3_quicksort_recursive_count.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_3_quicksort_recursive_count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Count(new[] { 1, 2, 3, 4 }));
+        }
+        private static int Count(IEnumerable<int> list)
+        {
+            if (!list.Any()) return 0;
+
+            return 1 + Count(list.Skip(1));
+        }
+    }
+}
