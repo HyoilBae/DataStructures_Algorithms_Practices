7fbe5aebfe07639585995da309d7b83ac7bb8446
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-04-26 12:22:45 -0700

    LeetCode_ContrainerWithMostWater

---
+++ LeetCode_ContainerWithMostWater/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_ContainerWithMostWater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+
+            int[] input = new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            Console.WriteLine(s.MaxArea(input));
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            int max = int.MinValue;
+            for (int i = 0; i < height.Length; i++)
+            {
+                for (int j = 0; j < height.Length; j++)
+                {
+                    int min = Math.Min(height[i], height[j]);
+                    max = Math.Max(max, min * (j - i));
+                }
+               
+            }
+
+            return max;
+        }
+    }
+}
