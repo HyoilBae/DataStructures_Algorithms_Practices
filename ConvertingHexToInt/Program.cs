a797277080da06240954a0e4daf837dee5661b70
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-04-26 11:23:20 -0700

    ConvertingHexToInt

---
+++ ConvertingHexToInt/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertingHexToInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "23";
+
+            Console.WriteLine(test[0]);
+
+            //string[] testArray = test.Split("");
+
+            //int intTest = Int32.Parse(test);
+            //Console.WriteLine(intTest);
+            //Console.WriteLine(intTest.GetType());
+        }
+    }
+}
