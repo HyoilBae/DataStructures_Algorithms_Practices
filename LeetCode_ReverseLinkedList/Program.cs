2dab9fb3a1fe95bfcb0f20b2cbaa8afe04c6025b
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-04-26 17:29:26 -0700

    add

--- LeetCode_ReverseLinkedList/Program.cs
+++ LeetCode_ReverseLinkedList/Program.cs
@@ -12,4 +12,35 @@ namespace LeetCode_ReverseLinkedList
         {
         }
     }
+
+    public class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            Stack<ListNode> stack = new Stack<ListNode>();
+            while (head != null)
+            {
+                stack.Push(head);
+                head = head.next;
+            }
+
+            ListNode dummy = new ListNode(-1);
+            head = dummy;
+            while (stack.Count != 0)
+            {
+                ListNode current = stack.Pop();
+                head.next = new ListNode(current.val);
+                head = head.next;
+            }
+
+            return dummy.next;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
 }
